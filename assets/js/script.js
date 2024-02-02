8b74d4a83d13ba91443126b83ef5f3f13a0b1beb
Aurore VERNET <aurore.vernet@live.com>
2024-02-02 15:35:36 +0100

    add design

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,41 +7,41 @@ let app = new Vue({
     el: "#app",
     data: {
         // Initialisation tableau d'objets genres
-        typesList : [],
+        genresList : [],
         // Initialisation tableau d'objets films selon le genre choisi
-        moviesList : [],
+        filmsList : [],
         // Variables permettant l'affichage draggable
         enabled : true,
         drag : false,
         // Récupération du genre sélectionné
-        typeSelected : "",
+        genreSelected : "",
         // Gérer affichage du vote
         displayVote : true,
         // Gérer affichage du bouton de fin de vote
         displaySubmitButton : false,
         // Gérer affichage du résultat du vote
-        displayResults : true,
+        displayResult : false,
         // Tableau qui récupère le vote enregistré dans le local storage
-        arrayResults : [],
+        arrayResult : [],
     },
     mounted() {
        // Récupération genres sur TMDB
         fetch(`https://api.themoviedb.org/3/genre/movie/list?language=fr&api_key=${KEY}`)
             .then(response => response.json())
-            .then(response => this.typesList = response.genres)
+            .then(response => this.genresList = response.genres)
             .catch(err => console.error(err));
     },
     methods: {
         // Méthode qui affiche les films selon la catégorie lorsque l'on clique sur celle-ci
-        showMovies(id) {
+        showFilms(id) {
             // Récupération des 20 films du genre sélectionné et trié selon leur popularité sur TMDB
             fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&language=fr-FR&sort_by=popularity.desc&with_genres=${id}&page=1&api_key=${KEY}`)
                 .then(response => response.json())
                 .then(response => {
                     // on stocke ces résultats dans la variable moviesList
-                    this.moviesList = response.results;
+                    this.filmsList = response.results;
                     // on splice le tableau pour ne garder que les 10 premiers 
-                    this.moviesList.splice(10,10)
+                    this.filmsList.splice(10,10)
                 })
                 .catch(err => console.error(err));
             this.displaySubmitButton = true;
@@ -49,23 +49,30 @@ let app = new Vue({
         // Méthode qui se lance au moment du clique sur le bouton "Terminer le vote"
         saveVote() {
             // Ajout d'un item dans le local storage du navigateur : le tableau avec les films classés + nom clé = catégorie
-            localStorage.setItem(this.typeSelected.name, JSON.stringify(this.moviesList));
+            localStorage.setItem(this.genreSelected.name, JSON.stringify(this.filmsList));
             // La section de vote est cachée
             this.displayVote = false;
             // Récupération item local storage et stockage dans une variable
-            this.arrayResults = JSON.parse(localStorage.getItem(this.typeSelected.name));
+            this.arrayResult = JSON.parse(localStorage.getItem(this.typeSelected.name));
             // Affichage section résultats
-            this.displayResults = true;
+            this.displayResult = true;
         },
         // Méthode qui se lance lorsque l'on clique sur une catégorie pour vérifier s'il existe un vote enregistré dans le local storage
         checkLocalStorage() { 
-            if (localStorage.getItem(this.typeSelected.name)){
+            if (localStorage.getItem(this.genreSelected.name)){
                 // si c'est le cas, on récupère le vote dans une variable
-                this.arrayResults = JSON.parse(localStorage.getItem(this.typeSelected.name));
+                this.arrayResult = JSON.parse(localStorage.getItem(this.genreSelected.name));
                 // on cache le vote et on affiche les résultats
                 this.displayVote = false;
-                this.displayResults = true;
+                this.displayResult = true;
            }
+        },
+        editVote() {
+            this.displayResult = false;
+            this.displayVote = true;
+        },
+        refresh() {
+            location.reload();
         }
     }
 });
